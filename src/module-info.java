d7dd759447fe89fe9b1a8aafc9ad09ae2800d88d
Sebas229 <jsriveraro24>
2024-03-08 07:17:34 -0500

    crear televisor

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Televisor1 {
+}
---
+++ src/televisor/Televisor.java
@@ -0,0 +1,22 @@
+package televisor;
+public class Televisor {
+	String marca;
+	
+	int tamaño;
+	
+	int volumen;
+	
+	int canal;
+	
+	String  entrada;
+	
+	boolean Encendido;
+	
+	
+	public void setEncendido(boolean encendido) {
+		this.Encendido = encendido;
+		
+		
+	}
+}	
+
